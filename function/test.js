f0def83564eb76bd89ba5a522195131f3f19686c
Mona Lisa <subastianhambali@gmail.com>
2025-05-10 19:49:17 +0700

    memperbaiki clean slug

---
+++ function/test.js
@@ -0,0 +1,12 @@
+function slugify(str) {
+  return decodeURIComponent(str)        // ubah %20 jadi spasi, dll
+    .replace(/\((\d{4})\)/, '-$1')      // ubah (2024) → -2024
+    .replace(/&/g, '')                  // hilangkan &
+    .replace(/\s+/g, '-')               // ganti semua spasi jadi -
+    .replace(/[^a-z0-9\-]/gi, '')       // hapus semua karakter aneh
+    .replace(/-+/g, '-')                // gabungkan double/triple - jadi satu
+    .replace(/^-|-$/g, '')              // hapus tanda - di awal/akhir
+    .toLowerCase();                    // ubah jadi lowercase
+}
+
+console.log(slugify('%20Deadpool%20&%20Wolverine%20(2024)'))
