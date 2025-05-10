9ae85955b29c047d573d8e7a021aaee162173967
Mona Lisa <subastianhambali@gmail.com>
2025-05-10 19:31:31 +0700

    memperbaiki clean slug

--- function/lk21.js
+++ function/lk21.js
@@ -76,9 +76,11 @@ const filmApik = {
 
 DownloadApik: async(slug)=>{
   const cleanedSlug = slug.toLowerCase()
-  .replace(/\s*\((\d{4})\)/, '-$1')    // ganti (2023) jadi -2023
-  .replace(/\s+/g, '-')                // ganti spasi dengan tanda -
-  .replace(/[^a-z0-9\-]/g, '');        // hilangkan semua kecuali huruf, angka, dan tanda minus
+    .replace(/\s*\((\d{4})\)/, '-$1')
+  .replace(/\s+/g, '-')
+  .replace(/[^a-z0-9\-]/gi, '')
+  .replace(/-+/g, '-')
+  .replace(/^-|-$/g, '');
   const response = await fetch(`https://filmapik.now/nonton-film-${cleanedSlug}-subtitle-indonesia/play`)
   const data = await response.text()
   const $ = cheerio.load(data)
