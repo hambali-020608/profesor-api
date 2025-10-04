790e031ad9a60885e3e48e48cfd739c25c667268
hambali-020608 <subastianhambali@gmail.com>
2025-10-05 00:44:23 +0700

    mengganti route dengan v1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// router.use("/youtube", require("./youtube.routes"));
 router.use("/api/spotify", require("./spotify.routes"));
 router.use("/api/tiktok", require("./tiktok.routes"));
 router.use("/api/youtube", require("./youtube.routes"));
 router.use("/api/movies", require("./movies.routes"));
-// router.use("/muslimai", require("./muslim.routes"));
 
 module.exports = router;
