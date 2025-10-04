9b262f9b67a9ce2eb7d66c9881e4bc350e19d1a1
hambali-020608 <subastianhambali@gmail.com>
2025-10-04 23:32:40 +0700

    memperbaiki routes movies

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const movieController = require("../controllers/movie.controller");
 
 
-router.get('/api/movies/v2/latest',movieController.justtalkLatest)
-router.get('/api/movies/v2/movies',movieController.justtalkMovies)
-router.get('/api/movies/v2/streaming',movieController.justtalkStreaming)
-router.get('/api/movies/v2/search',movieController.justtalkSearch)
+router.get('/v2/latest',movieController.justtalkLatest)
+router.get('/v2/movies',movieController.justtalkMovies)
+router.get('/v2/streaming',movieController.justtalkStreaming)
+router.get('/v2/search',movieController.justtalkSearch)
 
 
 module.exports = router;
